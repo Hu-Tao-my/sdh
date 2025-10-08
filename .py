afd036fce2d2783d15710ed3cd3723b41c0a3e2f
賀凊訢 <hanshe0509@gmail.com>
2025-10-08 17:53:03 +0800

    d

--- .py
+++ .py
@@ -1,1 +1,13 @@
-print ("hello world")
+a = " "
+while True:
+    n = int ( input( ))
+    if n != 0 :
+        for i in range ( 1 , n ):
+            if i % 7 == 0:
+                continue
+            a += str ( i )
+            a += " "
+        a += "\n"
+    else:
+        print (a)
+        break
